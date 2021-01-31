cd643a9817fd99c819c1cb882e13f986180ff7a6
Russell <russell.t.sherman@gmail.com>
2021-01-30 22:13:26 -0600

    installs

--- installs/Ω_finalize.zsh
+++ installs/Ω_finalize.zsh
@@ -1,4 +1,10 @@
-# Files in the zsh.d directory are executed by the lexicographical order of their file names.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# shellcheck disable=SC1090
+source "$DIR/../lib/sh"
+
+# Files in the iinstalld directory are executed by the lexicographical order of their file names.
+
 # This file is named Ω_finalize.zsh. The leading Ω (Capital Greek Omega) is needed to ensure this file executes after
 # other files that this function needs to be able to see.
 # This file should come next to last, followed only by Ω_overrides.zsh.
